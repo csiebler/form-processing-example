37e9555bdff970f0e9ee31e705bf9010ec2f6c16
Clemens Siebler <clemens.siebler@gmail.com>
2022-06-13 11:15:12 +0200

    Added Form Recognizer code

--- ProcessIncomingForms/__init__.py
+++ ProcessIncomingForms/__init__.py
@@ -1,9 +1,38 @@
+import os
 import logging
 
 import azure.functions as func
+from azure.identity import AzureCliCredential, ChainedTokenCredential, ManagedIdentityCredential
+from azure.ai.formrecognizer import FormRecognizerClient
+from azure.core.credentials import AzureKeyCredential
 
 
 def main(myblob: func.InputStream):
-    logging.info(f"Python blob trigger function processed blob \n"
-                 f"Name: {myblob.name}\n"
-                 f"Blob Size: {myblob.length} bytes")
+
+    logging.info(f"Python blob trigger function processed blob {myblob.uri}")
+    
+    endpoint = os.getenv("FORM_RECOGNIZER_ENDPOINT")
+    credential = ChainedTokenCredential(ManagedIdentityCredential(), AzureCliCredential())
+    
+    form_recognizer_client = FormRecognizerClient(
+        endpoint=endpoint,
+        credential=credential)
+    
+    poller = form_recognizer_client.begin_recognize_invoices_from_url(invoice_url=myblob.uri)
+    invoices = poller.result()
+    
+    minimum_confidences = {
+        "VendorName": 0.85,
+        "VendorAddress": 0.85,
+        "InvoiceDate": 0.85,
+        "InvoiceTotal": 0.85        
+    }
+    
+    for idx, invoice in enumerate(invoices):
+        print("--------Recognizing invoice #-------")
+        for field_name, min_confidence in minimum_confidences.items():
+            field = invoice.fields.get(field_name)
+            if field and field.confidence >= min_confidence:
+                print(f"Success: {field_name}={field.value} found ({field.confidence}>={min_confidence})")
+            else:
+                print(f"Error: {field_name} not found or confidence too low ({field.confidence}<{min_confidence})")
